ba402d2530c2a34d58cb10a7286a221db571b41c
menniia <mennia17@gmail.com>
2022-12-09 20:25:37 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+        return (0);
+}
